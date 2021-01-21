41bc3e012c8671176bf0c0213eba07a4a34df1b4
Troy Clarke <HX13978@wdxhx13978vcg.home.network>
2021-01-21 17:29:07 -0600

    StartGame&FristQuestionWithJS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello");
+let directions = document.querySelector('.directions')
+let start = document.querySelector('.start');
+let question = document.querySelectorAll('.q');
+
+let currentQIndex = 0
+
+start.addEventListener('click', startTrivia)
+
+function startTrivia(){
+    question[currentQIndex].style.display = 'block';
+    console.log('current', question[currentQIndex]);
+    directions.style.display= 'none';
+}
+
+document.querySelectorAll('.selection').forEach(choices => {
+    choices.addEventListener('click', answerSelection)
+  })
+
+function answerSelection (event) {
+    console.log('Hi');
+}
