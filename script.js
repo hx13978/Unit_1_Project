8ca4ae742a41a5a02dbb62551d8e79f332bcbc7d
Troy Clarke <HX13978@wdxhx13978vcg.home.network>
2021-01-22 16:56:50 -0600

    JSCodeToRunThroughQuestions

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-console.log("Hello");
+//console.log("Hello");
 let directions = document.querySelector('.directions')
 let start = document.querySelector('.start');
-let question = document.querySelectorAll('.q');
+let questions = document.querySelectorAll('.q');
 
-let currentQIndex = 0
+let currentQIndex = 0;
+let previousQIndex = -1;
 
-start.addEventListener('click', startTrivia)
+start.addEventListener('click', runTrivia)
 
-function startTrivia(){
-    question[currentQIndex].style.display = 'block';
-    console.log('current', question[currentQIndex]);
+function runTrivia() {
     directions.style.display= 'none';
+    questions[currentQIndex].style.display = 'block';
+    if(previousQIndex > -1) {
+    questions[previousQIndex].style.display = 'none'
+    }
+    currentQIndex ++;
+    previousQIndex ++;
+
+    //console.log('current', questions[currentQIndex]);   
 }
 
 document.querySelectorAll('.selection').forEach(choices => {
-    choices.addEventListener('click', answerSelection)
+    choices.addEventListener('click', runTrivia)
   })
 
-function answerSelection (event) {
-    console.log('Hi');
+document.querySelectorAll('.correct').forEach(correct => {
+    correct.addEventListener('click', scoreTally)
+})
+
+let score = 0
+
+function scoreTally() {
+score = score ++;
+console.log(score);
 }
+
+let restart = document.querySelector('.restart')
+restart.addEventListener('click', runTrivia);
+
