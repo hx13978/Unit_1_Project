1eaf203086ea6551187fb959cb75b7ff801c6288
Troy Clarke <HX13978@wdxhx13978vcg.home.network>
2021-01-23 11:24:40 -0600

    CorrectScoring

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 let directions = document.querySelector('.directions')
 let start = document.querySelector('.start');
 let questions = document.querySelectorAll('.q');
-
+let tryAgain = questions[questions.length - 1];
+let result = document.querySelector('.result');
+let score = 0;
 let currentQIndex = 0;
 let previousQIndex = -1;
 
@@ -12,12 +14,11 @@ function runTrivia() {
     directions.style.display= 'none';
     questions[currentQIndex].style.display = 'block';
     if(previousQIndex > -1) {
-    questions[previousQIndex].style.display = 'none'
+        questions[previousQIndex].style.display = 'none'
     }
+ 
     currentQIndex ++;
-    previousQIndex ++;
-    
-    //console.log('current', questions[currentQIndex]);   
+    previousQIndex ++;  
 }
 
 document.querySelectorAll('.selection').forEach(choices => {
@@ -31,30 +32,33 @@ document.querySelectorAll('.correct').forEach(correct => {
 let restart = document.querySelectorAll('.restart');
 restart[0].addEventListener('click', restartTrivia);
 
-//Logging each correct answer
-let score = 0
-
 function scoreTally() {
-score = score ++;
-console.log(score);
+    score ++;
+    //console.log("Tally " + score);
+    
+    if (score === 10) {
+        result.textContent = "Congratulations!!!  You got all ten questions correct!  Score: 100%";
+    }
+    else if (score === 9) {
+        result.textContent = "Great Job!  You got nine out of ten questions correct!  Score: 90%";
+    }
+    else if (score === 8 || score === 7) {
+        result.textContent = "Not bad!  You got seven to eight questions correct. Score: 70-80%";
+    }
+    else {
+        result.textContent = "Looks like you could sharpen your animal knowledge!  You got less than seven questions correct. Score: <= 60%";
+    };
 }
+  
 
 //Display score results and offer player to restart trivia game
 function restartTrivia () {
-    if (score === 10) {
-        alert("Congratulations!!!  You got all ten questions correct!  Score: 100%");
-    }
-    else if (score === 9) {
-        alert("Great Job!  You got nine out of ten questions correct!  Score: 90%");
-    }
-    else if (score === 8 || score === 7) {
-        alert('Not bad!  You got seven to eight questions correct. Score: 70-80%')
-    }
-    else {
-        alert('Looks like you could sharpen your animal knowledge!  You got less than seven questions correct. Score: <= 60%')
-    }
+    //console.log("reset score" + score);
+    //console.log("reset qIndex" + currentQIndex);
+    tryAgain.style.display = 'none';
     currentQIndex = 0;
     previousQIndex = -1;
     score = 0;
+    runTrivia();
    
 }
